f7c0e0508d4066a3b9af51f51c101f24b91394cd
tzyi <peterjuju99@gmail.com>
2020-02-21 14:07:23 +0800

    2020/02/21:Django超快速入門結束
    - 0005_將API資料顯示到網站.md
    - 0006_增加一個子頁面.md

--- 09_Web開發/Django/Django超快速入門/mysite/pizzawu/urls.py
+++ 09_Web開發/Django/Django超快速入門/mysite/pizzawu/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name="home"),
+    path('user/',views.user,name="user"),
 ]
 
--- 09_Web開發/Django/Django超快速入門/mysite/pizzawu/views.py
+++ 09_Web開發/Django/Django超快速入門/mysite/pizzawu/views.py
@@ -3,7 +3,61 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request,'home.html',{})
+
+    import requests
+    import json
+
+    api_request = requests.get("https://api.github.com/users?since=0")
+    api = json.loads(api_request.content)
+
+    return render(request,'home.html',{"api":api})
 
 
 
+
+def user(request):
+
+    
+    
+    if request.method == "POST":
+
+        import requests
+        import json
+        
+        user = request.POST['user']
+        user_req = requests.get("https://api.github.com/users/"+user)
+        user_info = json.loads(user_req.content)
+        
+        return render(request,'user.html',{'user':user,'user_info':user_info})
+    
+    else:
+        notfound = "請在搜索框中輸入要查詢的user..."
+        return render(request,'user.html',{'notfound':notfound})
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
